aa9f3568272db77d749855bcee301c30b586e31b
Blake Pfaff <blake.a.pfaff2@gmail.com>
2017-12-01 11:01:58 -0600

    adj base uri

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,9 +2,10 @@ require 'httparty'
 
 class Kele
   include HTTParty
+  base_uri "https://www.bloc.io/api/v1"
 
-  def initialize
-    @block_base = https://www.bloc.io/api/v1
+
+  def initialize()
     @auth = eyJ0eXAiOiJKV1QiLCJhhGciOiJIUzI1NiJ9.eyJhcGlfa2V5IjoiYTc2MDZkNTBhYjA3NDE4ZWE4ZmU5NzliY2YxNTM1ZjAiLCJ1c2VyX2lkIjoyMzAzMTExLCJuYW1lIjoiQmVuIE5lZWx5In0.3VXD-FxOoxaGXHu6vmL8g191bl5F_oKe9qj8Khmp9F0",
   end
 
