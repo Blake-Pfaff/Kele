7f0dbdf1c3528f693446423c6f2e0fccbd304782
Blake Pfaff <blake.a.pfaff2@gmail.com>
2017-12-03 12:26:24 -0600

    adds HTTParty and init method

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,15 +1,24 @@
+#make sure kele class has access to httparty
 require 'httparty'
 
 class Kele
+  #includes the httparty libary class as a module, gives access to httparty methods like "post" think inheritance-ish
   include HTTParty
-  base_uri "https://www.bloc.io/api/v1"
-
-
-  def initialize()
-    @auth = eyJ0eXAiOiJKV1QiLCJhhGciOiJIUzI1NiJ9.eyJhcGlfa2V5IjoiYTc2MDZkNTBhYjA3NDE4ZWE4ZmU5NzliY2YxNTM1ZjAiLCJ1c2VyX2lkIjoyMzAzMTExLCJuYW1lIjoiQmVuIE5lZWx5In0.3VXD-FxOoxaGXHu6vmL8g191bl5F_oKe9qj8Khmp9F0",
+  base_uri 'https://www.bloc.io/api/v1'
+  #called when "new" method is used.  Passes the email and PW in from the input
+  def initialize(email, password)
+    #self.class will envoke HTTPArty to use post method.  "sessions" is passed into the private method api_url and used as the end point.
+    #this will return an auth token, wil send 404 if return page does not exist
+    response = self.class.post(api_url("sessions"), body: { email: email, password: password })
+    # if you receive error code 404 display string
+    raise 'YOU SHALL NOT PASS' if response.code == 404
+    #returns JSON key called auth_token and assigns it to @auth_token
+    @auth_token = response["auth_token"]
   end
 
-
-
-
+  private
+    #sets the api_url used in init method
+    def api_url(endpoint)
+      "https://www.bloc.io/api/v1/#{endpoint}"
+    end
 end
