760fbb9a59f8d1be0a548ca177b6c4ddbdf3c298
Blake Pfaff <blake.a.pfaff2@gmail.com>
2017-12-04 15:58:25 -0600

    for real prolly the last refactor...prolly..

--- lib/kele.rb
+++ lib/kele.rb
@@ -11,7 +11,7 @@ class Kele
   def initialize(email, password)
     #self.class will envoke HTTPArty to use post method.  "sessions" is passed into the private method api_url and used as the end point.
     #this will return an auth token, wil send 404 if return page does not exist
-    response = self.class.post(api_url("sessions"), body: { email: email, password: password })
+    response = self.class.post("/sessions", body: { email: email, password: password })
     # if you receive error code 404 display string
     raise 'YOU SHALL NOT PASS' if response.code == 404
     #returns JSON key called auth_token and assigns it to @auth_token
@@ -19,14 +19,7 @@ class Kele
   end
 
   def get_me
-    response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
+    response = self.class.get("/users/me", headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
-
-  private
-
-    #sets the api_url used in init method
-    def api_url(endpoint)
-      "https://www.bloc.io/api/v1/#{endpoint}"
-    end
 end
